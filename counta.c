b604920de279ea3942a33864f15a7a850c1c2c48
hack2me <fack457766@gmail.com>
2024-11-21 20:04:57 +0530

    Create counta.c

---
+++ counta.c
@@ -0,0 +1,43 @@
+#include<conio.h>
+void main()
+{
+ int ch;
+ printf("Enter Digit: ");
+ scanf("%d",&ch);
+ switch(ch)
+ {
+case 0:
+ printf("Zero\n");
+ break;
+case 1:
+ printf("one\n");
+ break;
+case 2:
+ printf("Two\n");
+ break;
+case 3:
+ printf("Three\n");
+ break;
+case 4:
+ printf("Four\n");
+ break;
+case 5:
+ printf("Five\n");
+ break;
+case 6:
+ printf("Six\n");
+ break;
+case 7:
+ printf("Seven\n");
+ break;
+case 8:
+ printf("Eight\n");
+ break;
+case 9:
+ printf("Nine\n");
+ break;
+default:
+ printf("invalid digit\nPlease try again ....\n");
+ break;
+ }
+}
